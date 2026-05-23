a8f3616145464c90f43aaf10e257b16a001bd686
Deepthi <deepthinadar@gmail.com>
2026-05-23 17:32:49 +0530

    Initial commit

---
+++ finance_assistance_agent/agent.py
@@ -0,0 +1,45 @@
+from google.adk.agents import LlmAgent
+from google.adk.tools.agent_tool import AgentTool
+from typing import Dict
+from investment_plan_agent.agent import investment_plan_agent
+
+def get_user_personal_finance_details() -> Dict:
+    """
+    Gets users personal finance details like salary, expense and savings capacity.
+    """
+    return {
+        "salary": 50000,
+        "expense": {
+            "rent": 15000,
+            "food": 5000,
+            "transport": 3000,
+            "emi": 5000,
+        },
+        "savings": 20000,
+    }
+
+finance_assistance_agent = LlmAgent(
+    name="finance_assistance_agent",
+    model="gemini-2.5-flash-lite",
+    description="A simple finance assistance that helps user's finance goals.",
+    instruction="""You are friendly finance assistant. you can help answer user's generic questions on finance and help plan their finance goals, be more friendly and positive.
+    A simple finance assistant that helps with user's finance goals.",
+
+    You have two tools to use to complete your task.I
+
+    1. get_user_personal_finance_details This tool will give you the user's current finance details
+
+    2. investment_plan_agent - This tool can perform Google Search to get any latest information from websites and will be able to ask more details from the user and plan their savings goal.
+
+    ALWAYS use the investment_plan_agent with google_search tool when asked about:
+
+    - Stock prices (e.g., "Tesla stock price", "TSLA latest price")
+
+    - Market data, financial news, or company information
+
+    - ANY question containing words like "latest", "current", "today", "now", "recent"
+    
+    """,
+    tools=[AgentTool(investment_plan_agent),get_user_personal_finance_details],)
+
+root_agent = finance_assistance_agent
